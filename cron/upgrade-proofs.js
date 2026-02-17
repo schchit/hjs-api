83063d50fc8b97aa0d2544213bee783ad37cbb49
HJS · A Protocol for Responsibility Tracing <10815391+schchit@users.noreply.github.com>
2026-02-17 23:12:51 +0800

    Update upgrade-proofs.js

--- cron/upgrade-proofs.js
+++ cron/upgrade-proofs.js
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+// Copyright (c) 2026 Human Judgment Systems Foundation Ltd.
+
 const { Pool } = require('pg');
 const { upgradeProof } = require('../lib/ots-utils');
 const cron = require('node-cron');
